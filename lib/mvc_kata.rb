63c7c828fd7e92eacec61edeb8ef4991fa7c379c
moro <moronatural@gmail.com>
2011-08-17 00:02:57 +0900

    rename

--- lib/mvc_kata.rb
+++ lib/mvc_kata.rb
@@ -1,58 +1,7 @@
 # -*- coding: utf-8 -*-
 require 'dice'
-require 'living'
-require 'view'
+require 'mvc_kata/battle'
 
-class Battle
-  attr_reader :player, :enemy, :turn_count
-  def initialize(view_class)
-    @view = view_class.new(self)
-    @enemy_classes = [Slime, Dragon]
-    @player = Player.new([@view])
-    @turn_count = 0
-  end
+module MvcKata
 
-  def command
-    @turn_count += 1
-    @view.query_command
-    @view.get_command
-  end
-
-  def mainloop
-    while (@player.living? && @enemy.living?) do
-      case command
-      when :hoimi then @player.cure(8)
-      else @player.attack(@enemy)
-      end
-
-      wait
-
-      if @enemy.living?
-        @enemy.attack(@player)
-        wait
-      end
-    end
-  end
-
-  def encounter
-    @enemy = Dice.shuffle(@enemy_classes).new([@view])
-    @view.encounter
-
-    wait
-    mainloop
-
-    @player.living? ?  @view.finish_battle : @view.game_over
-    wait
-  end
-
-  def start
-    encounter()
-  end
-
-  private
-
-  def wait
-    @view.wait
-  end
 end
-
---
+++ lib/mvc_kata/battle.rb
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+require 'dice'
+require 'mvc_kata/models'
+require 'mvc_kata/views'
+
+class Battle
+  attr_reader :player, :enemy, :turn_count
+  def initialize(view_class)
+    @view = view_class.new(self)
+    @enemy_classes = [Slime, Dragon]
+    @player = Player.new([@view])
+    @turn_count = 0
+  end
+
+  def command
+    @turn_count += 1
+    @view.query_command
+    @view.get_command
+  end
+
+  def mainloop
+    while (@player.living? && @enemy.living?) do
+      case command
+      when :hoimi then @player.cure(8)
+      else @player.attack(@enemy)
+      end
+
+      wait
+
+      if @enemy.living?
+        @enemy.attack(@player)
+        wait
+      end
+    end
+  end
+
+  def encounter
+    @enemy = Dice.shuffle(@enemy_classes).new([@view])
+    @view.encounter
+
+    wait
+    mainloop
+
+    @player.living? ?  @view.finish_battle : @view.game_over
+    wait
+  end
+
+  def start
+    encounter()
+  end
+
+  private
+
+  def wait
+    @view.wait
+  end
+end
+
