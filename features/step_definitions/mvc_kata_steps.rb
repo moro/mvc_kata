147cd66bf7b91fee22671749f38bdb2bd7ffcc67
moro <moronatural@gmail.com>
2011-08-17 12:47:26 +0900

    fixup tests

---
+++ features/step_definitions/mvc_kata_steps.rb
@@ -0,0 +1,33 @@
+# coding: utf-8
+
+Before do
+  @aruba_io_wait_seconds = 0.01
+end
+
+def type_command(command)
+  type command
+  2.times { type "\n" }
+end
+
+Given /^以下のコマンドを入力する:$/ do |table|
+  @aruba_io_wait_seconds = 0.001
+  type "\n"
+  table.hashes.each {|row| type_command(row['コマンド']) }
+  type "\n"
+end
+
+Given /ランダム値を以下のとおり固定する:/ do |table|
+  v = table.hashes.first
+  write_file 'stub_dice.rb', <<-RUBY
+require 'dice'
+module Dice
+  def [](num) ; #{v['ダメージ']} ; end
+  def roll(arr); MvcKata::#{v['敵']} ; end
+end
+RUBY
+end
+
+Given "起動する" do
+  Given  %Q{`ruby -I../../lib -r./stub_dice.rb ../../bin/mvc_kata`を対話的に実行する}
+end
+
